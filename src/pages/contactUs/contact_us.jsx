2b869aadf1a1cca3ea654261f538eaf1deb7ad0d
Mahmoud Tarek <mah.algohry@gmail.com>
2023-05-03 03:11:16 +0300

    contact us

--- src/pages/contactUs/contact_us.jsx
+++ src/pages/contactUs/contact_us.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import "./icon-font.min.css"
 import "../../Styling/contact.css"
+import '../../imported/css/main.css'
+import '../../imported/css/util.css'
 import { useForm } from "react-hook-form";
+import background from '../../imported/images/image4.jpg'
+import mailIcon from '../../imported/images/icons/icon-email.png'
+import phoneIcon from '../../imported/images/icons/icons8-phone-22.png'
+import profileIcon from '../../imported/images/icons/icons8-administrator-male-22.png'
 
 const Contact_us = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
@@ -59,61 +66,108 @@ const Contact_us = () => {
   };
   return (
     <>
-      <div className='content'>
-        <div className='container'>
-          <div className="row justify-content-center mt-4">
-            <div className="col-lg-9">
-              <h1 className="mb-4 text-center interFont fs-1 ">Contact Us</h1>
-              <form onSubmit={handleSubmit(handleRegistration, handleError)}>
-                <div className="row g-3">
-                  <div className="col-md-6">
-                    <label htmlFor="your-name" className="form-label">Name<span className="text-danger">*</span></label>
-                    <input type="text" className="form-control" {...register('name', registerOptions.name)} id="your-name" name="your-name" />
-                    <small className="text-danger">
-                      {errors?.name && errors.name.message}
-                    </small>
-                  </div>
-                  <div className="col-md-6">
-                    <label htmlFor="your-phone" className="form-label">Phone<span className="text-danger">*</span></label>
-                    <input type="text" className="form-control" id="your-phone" {...register('phone', registerOptions.phone)} />
-                    <small className="text-danger">
-                      {errors?.phone && errors.phone.message}
-                    </small>
-                  </div>
-                  <div className="col-md-6">
-                    <label htmlFor="your-email" className="form-label">Email<span className="text-danger">*</span></label>
-                    <input type="email" className="form-control" {...register('email', registerOptions.email)} id="your-email" />
-                    <small className="text-danger">
-                      {errors?.email && errors.email.message}
-                    </small>
-                  </div>
-                  <div className="col-md-6">
-                    <label htmlFor="your-subject" className="form-label">Subject<span className="text-danger">*</span></label>
-                    <input type="text" className="form-control" id="your-subject" {...register('subject', registerOptions.subject)} />
-                    <small className="text-danger">
-                      {errors?.subject && errors.subject.message}
-                    </small>
-                  </div>
-                  <div className="col-12">
-                    <label htmlFor="your-message" className="form-label">Message<span className="text-danger">*</span></label>
-                    <textarea className="form-control" id="your-message" {...register('message', registerOptions.message)} rows="5" ></textarea>
-                    <small className="text-danger">
-                      {errors?.message && errors.message.message}
-                    </small>
-                  </div>
-                  <div className="col-12">
-                    <div className="row">
-                      <div className="col-md-6">
-                        <button data-res="<?php echo $sum; ?>" type="submit" className="btn primarybg text-light w-100 fw-bold" >Send</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
+      <section className="bg-img1 txt-center p-lr-15 p-tb-92 contact__bk" 
+      style={{ backgroundImage: 'url(' + background + ')',height:"45vh" }}>
+		      <h2 className="ltext-105 cl0 txt-center txt__mar">
+			    Contact
+		      </h2>
+	  </section>	
+
+
+    <section className="bg0 p-t-104 p-b-116">
+		<div className="container">
+			<div className="flex-w flex-tr">
+				<div className="size-210 bor10 p-lr-70 p-t-55 p-b-70 p-lr-15-lg w-full-md">
+					<form>
+						<h4 className="mtext-105 cl2 txt-center p-b-30">
+							Send Us A Message
+						</h4>
+
+            <div className="bor8 m-b-20 how-pos4-parent">
+							<input className="stext-111 cl2 plh3 size-116 p-l-62 p-r-30" type="text" name="userName" placeholder="Your Name"/>
+							<img className="how-pos4 pointer-none " src={profileIcon} alt="ICON"/>
+						</div>
+
+
+						<div className="bor8 m-b-20 how-pos4-parent">
+							<input className="stext-111 cl2 plh3 size-116 p-l-62 p-r-30" type="text" name="email" placeholder="Your Email Address"/>
+							<img className="how-pos4 pointer-none" src={mailIcon} alt="ICON"/>
+						</div>
+
+            <div className="bor8 m-b-20 how-pos4-parent">
+							<input className="stext-111 cl2 plh3 size-116 p-l-62 p-r-30" type="text" name="mobile" placeholder="Your Phone Number"/>
+							<img className="how-pos4 pointer-none " src={phoneIcon} alt="ICON"/>
+						</div>
+
+            
+
+						<div class="bor8 m-b-30">
+							<textarea className="stext-111 cl2 plh3 size-120 p-lr-28 p-tb-25" name="msg" placeholder="How Can We Help?"></textarea>
+						</div>
+
+						<button className="flex-c-m stext-101 cl0 size-121 bg3 bor1 hov-btn3 p-lr-15 trans-04 pointer">
+							Submit
+						</button>
+					</form>
+				</div>
+
+				<div className="size-210 bor10 flex-w flex-col-m p-lr-93 p-tb-30 p-lr-15-lg w-full-md">
+					<div className="flex-w w-full p-b-42">
+						<span className="fs-18 cl5 txt-center size-211">
+							<span className="lnr lnr-map-marker"></span>
+						</span>
+
+						<div class="size-212 p-t-2">
+							<span class="mtext-110 cl2">
+								Address
+							</span>
+
+							<p class="stext-115 cl6 size-213 p-t-18">
+								Coza Store Center 8th floor, 379 Hudson St, New York, NY 10018 US
+							</p>
+						</div>
+					</div>
+
+					<div class="flex-w w-full p-b-42">
+						<span class="fs-18 cl5 txt-center size-211">
+							<span class="lnr lnr-phone-handset"></span>
+						</span>
+
+						<div class="size-212 p-t-2">
+							<span class="mtext-110 cl2">
+								Lets Talk
+							</span>
+
+							<p class="stext-115 cl1 size-213 p-t-18">
+								+1 800 1236879
+							</p>
+						</div>
+					</div>
+
+					<div class="flex-w w-full">
+						<span class="fs-18 cl5 txt-center size-211">
+							<span class="lnr lnr-envelope"></span>
+						</span>
+
+						<div class="size-212 p-t-2">
+							<span class="mtext-110 cl2">
+								Sale Support
+							</span>
+
+							<p class="stext-115 cl1 size-213 p-t-18">
+								contact@example.com
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>	
+
+
+
+
+    
     </>
   )
 }
