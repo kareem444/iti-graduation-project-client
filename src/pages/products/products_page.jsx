ceec0acb448f649765672181ca6cf112903dc246
ashrafahmedthabet <ashraf.a.thabet@gmail.com>
2023-05-03 17:46:55 +0300

    products page

--- src/pages/products/products_page.jsx
+++ src/pages/products/products_page.jsx
@@ -1,111 +1,154 @@
 import React from 'react'
-import '../../Styling/sidebars.css'
+// import '../../Styling/sidebars.css'
 import Footersection from '../../components/footer_component/footer_component'
 import { RepoGetAllProducts } from '../../repositories/product.repo'
 import { NavLink } from 'react-router-dom'
+import "./product_page.css"
+import product1 from "./components/images/product-01.jpg"
 const Products = () => {
 
-  const { data: products, isLoading } = RepoGetAllProducts()
+  // const { data: products, isLoading } = RepoGetAllProducts()
 
   return (
     <>
-      <div className='content'>
-        <div className='container'>
-          <h2 className='fs-1 text-center mt-2'>Products</h2>
-          <div className='row mt-3 col-sm-12'>
-            {/* filter */}
-            <main className='d-flex col-md-3 col-sm-12 col-xs-12 flex-nowrap mt-4'>
-              <div className='flex-shrink-0 p-3' style={{ width: '100%' }}>
-                <p className='d-flex align-items-center pb-3 mb-3 link-body-emphasis text-decoration-none border-bottom'>
-                  <span className='fs-5 fw-semibold '>Filters</span>
-                </p>
-                <ul className='list-unstyled ps-0'>
-                  <li className='mb-1'>
-                    <button
-                      className='btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed'
-                      data-bs-toggle='collapse'
-                      data-bs-target='#home-collapse'
-                      aria-expanded='true'
-                    >
-                      Locations
-                    </button>
-                    <div className='collapse show' id='home-collapse'>
-                      <ul className='btn-toggle-nav list-unstyled fw-normal pb-1 small'>
-                        <li>
-                          <div className='ms-4 form-check'>
-                            <input
-                              className='form-check-input'
-                              type='checkbox'
-                              id='giza'
-                            ></input>
-                            <label className='form-check-label' htmlFor='giza'>
-                              Giza
-                            </label>
-                          </div>
-                        </li>
-                        <li>
-                          <div className='ms-4 form-check'>
-                            <input
-                              className='form-check-input'
-                              type='checkbox'
-                              id='cairo'
-                            ></input>
-                            <label className='form-check-label' htmlFor='cairo'>
-                              Cairo
-                            </label>
-                          </div>
-                        </li>
-                        <li>
-                          <div className='ms-4 form-check'>
-                            <input
-                              className='form-check-input'
-                              type='checkbox'
-                              id='alex'
-                            ></input>
-                            <label className='form-check-label' htmlFor='alex'>
-                              Alex
-                            </label>
-                          </div>
-                        </li>
-                      </ul>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </main>
-            <div className='col-md-9 col-sm-12 py-2'>
-              <div className='row my-3 justify-content-between'>
-                {products?.map(product => {
-                  return (
-                    <div className='col-md-4'>
-                      <div className='card mb-5'>
-                        <img
-                          src={product?.thumbImage ?? ""}
-                          className=' card-img-top p-1'
-                          alt='...'
-                        />
-                        <div className='card-body'>
-                          <h5 className='card-title'>{product?.title}</h5>
-                          <p className='card-text'>
-                            {product?.description.slice(0, 55).concat('...')}
-                          </p>
-                          <NavLink
-                            to={`/productdetails/${product['_id']}`}
-                            className='btn btn-primary text-white'
-                          >
-                            See More Details
-                          </NavLink>
-                        </div>
-                      </div>
-                    </div>
-                  )
-                })}
-              </div>
+
+  <div className="container mt-5">
+    <div className="flex-w flex-sb-m p-b-52">
+      <div className="flex-w flex-l-m filter-tope-group m-tb-10">
+        <button className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5 how-active1" data-filter="*">
+          All Products
+        </button>
+        <button className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".women">
+          Cairo
+        </button>
+        <button className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".men">
+          Giza
+        </button>
+        <button className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".bag">
+          Alex
+        </button>
+      </div>
+      <div className=" m-tb-10 d-flex col-12 col-md-4 ">
+        <input type="text" className='border w-100' placeholder='Search'/>
+      <button type="button" class="btn btn-primary py-2 product_page_button_search px-4 ">
+      <i class="bi bi-search"></i>
+            </button>
+      </div>
+      <div className="dis-none panel-search w-full p-t-10 p-b-15">
+        <div className="bor8 dis-flex p-l-15">
+          <button className="size-113 flex-c-m fs-16 cl2 hov-cl1 trans-04">
+            <i className="zmdi zmdi-search" />
+          </button>
+          <input className="mtext-107 cl2 size-114 plh2 p-r-15" type="text" name="search-product" placeholder="Search" />
+        </div>	
+      </div>
+    </div>
+    <div className="row isotope-grid">
+      <div className="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women">
+        <div className="block2">
+          <div className="block2-pic hov-img0">
+            <img src={product1} alt="IMG-PRODUCT" />
+            <a href="#" className="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
+              More Details
+            </a>
+          </div>
+          <div className="block2-txt flex-w flex-t p-t-14">
+            <div className="block2-txt-child1 flex-col-l ">
+              <a href="product-detail.html" className="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
+                Esprit Ruffle Shirt
+              </a>
+              <span className="stext-105 cl3">
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <span>(5)</span>
+              </span>
             </div>
           </div>
         </div>
       </div>
-      <Footersection />
+      <div className="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women">
+        
+        <div className="block2">
+          <div className="block2-pic hov-img0">
+            <img src={product1} alt="IMG-PRODUCT" />
+            <a href="#" className="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
+              More Details
+            </a>
+          </div>
+          <div className="block2-txt flex-w flex-t p-t-14">
+            <div className="block2-txt-child1 flex-col-l ">
+              <a href="product-detail.html" className="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
+                Herschel supply
+              </a>
+              <span className="stext-105 cl3">
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              </span>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <div className="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item men">
+        
+        <div className="block2">
+          <div className="block2-pic hov-img0">
+            <img src={product1} alt="IMG-PRODUCT" />
+            <a href="#" className="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
+              More Details
+            </a>
+          </div>
+          <div className="block2-txt flex-w flex-t p-t-14">
+            <div className="block2-txt-child1 flex-col-l ">
+              <a href="product-detail.html" className="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
+                Only Check Trouser
+              </a>
+              <span className="stext-105 cl3">
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              </span>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <div className="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women">
+        <div className="block2">
+          <div className="block2-pic hov-img0">
+            <img src={product1} alt="IMG-PRODUCT" />
+            <a href="#" className="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
+              More Details
+            </a>
+          </div>
+          <div className="block2-txt flex-w flex-t p-t-14">
+            <div className="block2-txt-child1 flex-col-l ">
+              <a href="product-detail.html" className="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
+                Classic Trench Coat
+              </a>
+              <span className="stext-105 cl3">
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              </span>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
     </>
   )
 }
