fe82af9b0c4f2d6df6579c05613b009552ebcbe9
kareem444 <kareem963221@gmail.com>
2023-05-04 04:44:01 +0300

    fix product details bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './imported/styles/style.scss';
 import './index.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import MainRouter from './router/main_router';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
@@ -14,7 +16,6 @@ import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 import "@fortawesome/fontawesome-svg-core"
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { CookiesProvider } from 'react-cookie';
-
 i18nextHandler()
 
 const queryClient = new QueryClient()
