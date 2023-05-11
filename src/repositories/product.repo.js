fbd7f42e6ff6bc241bb59d97b90aa36e1e61bf3c
kareem444 <kareem963221@gmail.com>
2023-05-11 17:02:27 +0300

    edit product repo

--- src/repositories/product.repo.js
+++ src/repositories/product.repo.js
@@ -1,3 +1,66 @@
+// import {
+//   useMutation,
+//   useQuery,
+//   useQueryClient,
+// } from "@tanstack/react-query";
+// import {
+//   ENDPOINT_MYPRODUCTS,
+//   ENDPOINT_PRODUCTS,
+//   ENDPOINT_ORDERS_SELLER,
+// } from "../utils/constants/endpoints.constants";
+// import {
+//   KEY_REPO_GET_ALL_MY_PRODUCTS,
+//   KEY_REPO_GET_ALL_PRODUCTS,
+//   KEY_REPO_GET_One_PRODUCT,
+// } from "../utils/constants/queries_keys.constants";
+// import AxiosApiHelper from "../helper/axios_api.helper";
+// // import {} from "./../utils/constants/endpoints.constants";
+
+// export const RepoGetAllProducts = () => {
+//   return useQuery([KEY_REPO_GET_ALL_PRODUCTS], () =>
+//     AxiosApiHelper.get(ENDPOINT_PRODUCTS)
+//   );
+// };
+
+// export const RepoGetAllMyProducts = () => {
+//   return useQuery([KEY_REPO_GET_ALL_MY_PRODUCTS], () =>
+//     AxiosApiHelper.get(ENDPOINT_MYPRODUCTS)
+//   );
+// };
+
+// export const RepGetOneProduct = (productId) => {
+//   return useQuery(
+//     [KEY_REPO_GET_One_PRODUCT, productId],
+//     () =>
+//       AxiosApiHelper.get(
+//         ENDPOINT_PRODUCTS + "/" + productId
+//       )
+//   );
+// };
+
+// export const RepoCreateProduct = () => {
+//   return useMutation(
+//     async (data) =>
+//       await AxiosApiHelper.postNewProduct(
+//         ENDPOINT_PRODUCTS,
+//         data
+//       )
+//   );
+// };
+
+// export const RepoUpdateProduct = () => {
+//   return useMutation((id, data) =>
+//     AxiosApiHelper.patch(ENDPOINT_PRODUCTS + "/" + id, data)
+//   );
+// };
+
+// export const RepoDeleteProduct = () => {
+//   return useMutation((id) =>
+//     AxiosApiHelper.delete(ENDPOINT_PRODUCTS + "/" + id)
+//   );
+// };
+
+
 import {
   useMutation,
   useQuery,
@@ -14,8 +77,8 @@ import {
   KEY_REPO_GET_One_PRODUCT,
 } from "../utils/constants/queries_keys.constants";
 import AxiosApiHelper from "../helper/axios_api.helper";
-// import {} from "./../utils/constants/endpoints.constants";
-
+import { useDispatch } from "react-redux";
+import { showErrorAlert, showSuccessAlert } from "../redux/global/global.reducer";
 export const RepoGetAllProducts = () => {
   return useQuery([KEY_REPO_GET_ALL_PRODUCTS], () =>
     AxiosApiHelper.get(ENDPOINT_PRODUCTS)
@@ -38,24 +101,60 @@ export const RepGetOneProduct = (productId) => {
   );
 };
 
+export const RepoUpdateProduct = () => {
+  const dispatch= useDispatch()
+
+  return useMutation(
+      data => {
+          return AxiosApiHelper.patch(`${ENDPOINT_PRODUCTS}/${data["id"]}`,data["data"], {
+              'Content-Type': 'multipart/form-data'
+          })
+      },{
+        onSuccess: () => {
+            dispatch(showSuccessAlert("Product Updated successfully"));
+        },
+        onError: (error) => {
+            dispatch(showErrorAlert(error));
+        },
+    }
+  )
+}
 export const RepoCreateProduct = () => {
+  const dispatch= useDispatch()
   return useMutation(
     async (data) =>
       await AxiosApiHelper.postNewProduct(
         ENDPOINT_PRODUCTS,
         data
-      )
-  );
+      ),{
+        onSuccess: () => {
+        dispatch(showSuccessAlert("Product created successfully"));
+    },
+    onError: (error) => {
+        dispatch(showErrorAlert(error));
+    },
+  }
+  )
 };
 
-export const RepoUpdateProduct = () => {
+
+export const RepoUpdateeProduct = () => {
   return useMutation((id, data) =>
     AxiosApiHelper.patch(ENDPOINT_PRODUCTS + "/" + id, data)
   );
 };
 
 export const RepoDeleteProduct = () => {
+  const dispatch= useDispatch()
+
   return useMutation((id) =>
-    AxiosApiHelper.delete(ENDPOINT_PRODUCTS + "/" + id)
+    AxiosApiHelper.delete(ENDPOINT_PRODUCTS + "/" + id),{
+      onSuccess: () => {
+          dispatch(showSuccessAlert("Product Deleted successfully"));
+      },
+      onError: (error) => {
+          dispatch(showErrorAlert(error));
+      },
+  }
   );
 };
